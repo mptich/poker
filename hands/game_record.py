f46e74ad2bc487c6ad9bcbc9e883dbe6414afeae
Misha Orel <morel@spirorobotics.com>
2024-11-17 04:54:43 +0000

    Class GameRecord - initial version

---
+++ hands/game_record.py
@@ -0,0 +1,127 @@
+import re
+from datetime import datetime
+import pytz
+
+FirstLine = re.compile(r"PokerStars Hand \#(\d+): +Hold'em No Limit \((.+)\).*\[(\d{4}\/\d{2}\/\d{2} \d{2}\:\d{2}\:\d{2}) ([A-Z]+)\].*")
+
+SBBBDict = {"$0.50/$1.00 USD": ("USD", 0.5, 1.),
+         "$1.00/$2.00 USD": ("USD", 1., 2.)}
+
+TimeZoneDict = {"ET": "US/Eastern"}
+
+SecondLine = re.compile(r"Table .* Seat \#(\d+) is the button")
+
+SittingOutLine = re.compile(r".+is sitting out")
+SitsOutLine = re.compile(r".+sits out")
+
+PlayerLine = re.compile(r"Seat (\d+): (.+) \(.+([\d.]+) in chips\)")
+
+SmallBlindLine = re.compile(r"(.+)\: 
+
+
+class GameRecord:
+ WasteLines = [SittingOutLine, StisOutLine]
+
+ def ParsingErr(self):
+  return f"Failed parsing line {self.ln + self.ln_offset}"
+
+ def SkipWaist(self):
+  while True:
+   if self.ln >= len(self.lines):
+    return
+   skip = False
+   for l in self.WasteLines:
+    if l.match(self.lines[self.ln]) is not None:
+     skip = True
+     break
+   if not skip:
+    return
+   self.ln += 1
+   
+
+ def __init__(self, lines: list, line_number: int): 
+  self.lines = lines
+  self.ln_offset = line_number
+  self.ln = 0
+  self.MatchFirstLine()
+
+  self.ln = 1
+  self.MatchSecondLine()
+
+  self.players = {}
+  self.ln = 2
+  while(self.MatchPlayerLine()):
+   self.ln += 1
+  self.ArrangePlayers()
+
+  self.MatchSmallBlindLine()
+
+  self.ln += 1
+  self.MatchBigBlindLine()
+
+
+ def MatchFirstLine(self):
+  m = FirstLine.match(self.lines[self.ln])
+  assert m is not None, self.ParsingErr()
+  self.hand_number = int(m.group(1))
+  assert m.group(2) in SBBBDict, self.ParsingErr()
+  tup = SBBBDict[m.group(2)]
+  self.currency = tup[0]
+  self.sb_fee = tup[1]
+  self.bb_fee = tup[2]
+
+  assert m.group(4) in TimeZoneDict, self.ParsingErr()
+  self.GenerateUtcTimestamp(m.group(3), TimeZoneDict[m.group(4)])
+
+ def ParseSecondLine(self):
+  m = SecondLine.match(self.lines[self.ln])
+  assert m is not None, self.ParsingErr()
+  self.button = int(m.group(1))
+
+ def ParsePlayerLine(self):
+  self.SkipWaste()
+  m = PlayerLine.match(self.lines[self.ln])
+  if m is None:
+   return False
+  seat = int(m.group(1))
+  assert seat not in self.players, self.ParsingErr()
+  self.players[seat] = (m.group(2), round(float(m.group(3), 2)))
+  return True
+
+ def ArrangePlayers(self):
+  assert len(self.players) >= 2, self.ParsingErr()
+  assert len(self.players) <= 20, self.ParsingErr()
+  assert self.button in self.players, self.ParsingErr()
+  seats = sorted(list(self.players.keys()))
+  bindx = seats.indexof(button)
+
+  self.names = []
+  self.in_chips = {}
+  self.seat = {}
+  indx = bindx + 1
+  while True:
+   if indx >= len(seats):
+    indx = 0
+   tup = self.players[seats[indx]]
+   name = tup[0]
+   self.names.append(name)
+   self.in_chips[name] = tup[1] 
+   self.seat[name] = seats[indx]
+   if indx == bindx:
+    break
+   indx += 1
+   
+  self.players = None
+
+ def self.MatchSmallBlindLine(self):
+  self.SkipWaste()
+  prodolzhit'
+
+ def GenerateUtcTimestamp(self, tstr, tzstr):
+  tz = pytz.timezone(tzstr)
+  dt = datetime.strptime(tstr, "%Y/%m/%d %H:%M:%S").replace(tzinfo=tz)
+  self.timestamp = dt.astimezone(pytz.utc)
+  
+print('here')
+a=GameRecord(["PokerStars Hand #253306280353:  Hold'em No Limit ($0.50/$1.00 USD) - 2024/11/08 4:37:04 WET [2024/11/07 23:37:04 ET]"], 100)
+  
